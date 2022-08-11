682b98245dc69bc045c2315a65c1c528d9735c65
xyzhang7 <xzhang0980@gmail.com>
2022-08-11 18:16:56 +0800

    feat: DP subsequence * longest fibonacci sequence DP * TODO: use hashmap/divide and conquer to improve time complexity

--- src/py/dp/l873_longest_fibonacci_subsequence.py
+++ src/py/dp/l873_longest_fibonacci_subsequence.py
@@ -7,6 +7,7 @@ sequence, return the length of the longest Fibonacci-like subsequence of arr. If
 A subsequence is derived from another sequence arr by deleting any number of elements (including none) from arr,
 without changing the order of the remaining elements. For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].
 """
+from typing import List
 
 
 def lenLongestFibSubseq(A):
@@ -32,6 +33,28 @@ def lenLongestFibSubseq(A):
     return ans if ans >= 3 else 0
 
 
+def lenLongestFibSubseqDP(arr: List[int]) -> int:
+    """
+    DP:
+    if arr[k] + arr[i] == arr[j]:
+        dp[i][j] = max(dp[k][i] + 1) for k in range(0, i)
+    """
+    N = len(arr)
+    ans = 0
+    dp = [[2] * (N) for _ in range(N)]
+
+    for i in range(1, N):
+        for j in range(i + 1, N):
+            if j >= i * i:
+                break
+            for k in range(i):
+                if arr[k] + arr[i] == arr[j]:
+                    dp[i][j] = max(dp[k][i] + 1, dp[i][j])
+
+            ans = max(ans, dp[i][j])
+    return ans
+
+
 if __name__ == "__main__":
-    A = [2,4,7,8,9,10,14,15,18,23,32,50]
-    lenLongestFibSubseq(A)
+    A = [1,2,3,4,5,6,7,8]
+    lenLongestFibSubseqDP(A)
