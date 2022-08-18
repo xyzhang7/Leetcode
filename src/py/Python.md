7fcabdab31094743ce9a517c380bfa08b82eadd7
xyzhang7 <xzhang0980@gmail.com>
2022-08-18 21:58:28 +0800

    feat: DP Path Problems * LC 1301

--- src/py/Python.md
+++ src/py/Python.md
@@ -13,6 +13,7 @@ if (x & 1): pass      # odd number if x & 1 == 1; even otherwise
 * Math Package
 ```python
 from math import log2, ceil, floor
+int(1e10) == 10 ** 10       # 10^10
 ```
 
 * Max/Min
@@ -25,7 +26,7 @@ MAX = float('inf')                          # = Integer.MAX_VALUE
 k_smallest = heapq.nsmallest(k, array)      # find k smallest in array (详见source code)
 ```
 
-* Iteration
+* Iteration and Slice
 
 ```python
 from itertools import product
@@ -33,6 +34,14 @@ m, n = 0, 0
 for i, j in product(range(m), range(n)):    # = for i in range(m):
     pass                                    #       for j in range(n): 
     
+arr = [1, 2, 3, 4]                          # given a string/list/tuple, slice the object in the reverse order
+a = '1234'                                  # and the slice will just take a copy of each original element
+arr[::-1]                                   # modify the slice will not change the original object
+a[::-1]
+
+board = [[0] * 10 for _ in range(10)]       # = if board[i][j] == 'S' or board[i][j] == 'X' and ...
+if board[i][j] in 'SXE':                    # given a board of characters, it's Start position is 'S'
+    pass                                    # and its end position is 'E', obstacle is 'X'                                                                          
 ```
 
 * Char to Integer
--- src/py/dp/l1575_count_all_possible_routes.py
+++ src/py/dp/l1575_count_all_possible_routes.py
@@ -109,6 +109,96 @@ def findPathsDP(m: int, n: int, maxMove: int, startRow: int, startColumn: int) -
         ans = (ans + dpCurr[startRow+1][startColumn+1]) % 1000000007
     return ans
 
+def pathsWithMaxScore(board: List[str]) -> List[int]:
+    """
+    LC1301 Number of Paths with Max Score
+    You are given a square board of characters. You can move on the board starting at the bottom right square marked
+    with the character 'S'.
+
+    You need to reach the top left square marked with the character 'E'. The rest of the squares are labeled either
+    with a numeric character 1, 2, ..., 9 or with an obstacle 'X'. In one move you can go up, left or up-left (
+    diagonally) only if there is no obstacle there.
+
+    Return a list of two integers: the first integer is the maximum sum of numeric characters you can collect,
+    and the second is the number of such paths that you can take to get that maximum sum, taken modulo 10^9 + 7.
+
+    In case there is no path, return [0, 0].
+    """
+    N = len(board)
+
+    def to_num(grid):
+        start_char, end_char, obstacle_char = 'S', 'E', 'X'
+        if grid == start_char or grid == end_char:
+            return 0
+        elif grid == obstacle_char:
+            return -1
+        elif '1' <= grid <= '9':
+            return int(grid)
+        else:
+            return -2
+
+    # numeric board store the max value gaid from starting point to each grid
+    numeric_board = [[0] * (N + 1) for _ in range(N + 1)]
+    for i in range(N):
+        for j in range(N):
+            numeric_board[i][j] = to_num(board[i][j])
+            if numeric_board[i][j] == -2:
+                return [0, 0]
+
+    # dp store the number of paths from starting point to each grid
+    dp = [[0] * (N + 1) for _ in range(N + 1)]
+    dp[N-1][N-1] = 1
+
+    for i in range(N - 1, -1, -1):
+        for j in range(N - 1, -1, -1):
+            if (i == N - 1 and j == N - 1) or numeric_board[i][j] == -1:
+                continue
+
+            curr_max = max(numeric_board[i][j + 1], numeric_board[i + 1][j], numeric_board[i + 1][j + 1])
+            if curr_max == -1:
+                continue
+
+            numeric_board[i][j] += curr_max
+            if numeric_board[i][j + 1] == curr_max:
+                dp[i][j] += dp[i][j + 1]
+            if numeric_board[i + 1][j] == curr_max:
+                dp[i][j] += dp[i + 1][j]
+            if numeric_board[i + 1][j + 1] == curr_max:
+                dp[i][j] += dp[i + 1][j + 1]
+    if dp[0][0] == 0:
+        return [0, 0]
+    return [numeric_board[0][0], dp[0][0]]
+
+
+def pathsWithMaxScoreSol(board):
+    """
+    LC 1301 Best and most elegant solution @lee215
+    T: O(n^2), S: O(n^2)
+    """
+    n, mod = len(board), 10**9 + 7
+
+    # dp[x][y][0] is the maximum value to this cell,
+    # dp[x][y][1] is the number of paths.
+    dp = [[[-10**5, 0] for j in range(n+1)] for i in range(n+1)]
+    dp[n-1][n-1] = [0, 1]
+    for x in range(n)[::-1]:
+        for y in range(n)[::-1]:
+            if board[x][y] in 'XS': continue
+            for i, j in [[0, 1], [1, 0], [1, 1]]:
+                if dp[x][y][0] < dp[x+i][y+j][0]:
+                    dp[x][y] = [dp[x+i][y+j][0], 0]
+                if dp[x][y][0] == dp[x+i][y+j][0]:
+                    dp[x][y][1] += dp[x+i][y+j][1]
+
+            # add the board number in grid [x, y] if it is not the 'end' cell
+            dp[x][y][0] += int(board[x][y]) if x or y else 0
+
+    # if the number of maximum ways from start to end is 0, then return [0, 0]
+    return [dp[0][0][0] if dp[0][0][1] else 0, dp[0][0][1] % mod]
+
+
 
 if __name__ == "__main__":
-    findPathsImpl(3, 4, 3, 0, 0)
+    board1 = ["E23","2X2","12S"]
+    board2 = ["E11","XXX","11S"]
+    pathsWithMaxScore(board1)
